c1210f294e4b4b8a8e462492d412aa94be6f687e
renalazy <renalazyy@gmail.com>
2021-03-09 01:16:18 +0700

    Initial Commit

---
+++ lib/CustomDialog.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class CustomDialog extends StatelessWidget {
+  final String title, description, buttonText;
+  final AssetImage image;
+
+  CustomDialog({this.title, this.description, this.buttonText, this.image});
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: dialogContent(context),
+    );
+  }
+
+  dialogContent(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.only(
+            top: 100.0,
+            bottom: 16,
+            left: 16,
+            right: 16,
+          ),
+          margin: EdgeInsets.only(top: 16),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(17),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10.0,
+                    offset: Offset(0.0, 10.0))
+              ]),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 60),
+              Text(
+                title,
+                style: TextStyle(
+                  fontFamily: 'MontserratAlternates',
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 24.0),
+              Text(
+                description,
+                style: TextStyle(
+                    fontFamily: 'MontserratAlternates', fontSize: 16.0),
+              ),
+              SizedBox(height: 24.0),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: FlatButton(
+                  color: Color(0xff6C63FF),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(9.0)),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    buttonText,
+                    style: TextStyle(
+                      fontFamily: 'MontserratAlternates',
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 16,
+          right: 16,
+          child: CircleAvatar(
+            backgroundColor: Colors.blueAccent,
+            radius: 85,
+            backgroundImage: image,
+          ),
+        ),
+      ],
+    );
+  }
+}
