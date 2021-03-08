df35636e2ecf5a623fbea413930a87d67cad2315
renalazy <renalazyy@gmail.com>
2021-03-09 01:19:25 +0700

    Initial Commit

---
+++ lib/registerScreen.dart
@@ -0,0 +1,228 @@
+import 'package:eltravel/Animation/FadeAnimation.dart';
+import 'package:eltravel/homeScreen.dart';
+import 'package:eltravel/loginScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(RegisterScreen());
+  });
+}
+
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RegisterPage(),
+      routes: {
+        '/loginscreen': (_) => new LoginScreen(),
+        '/homescreen': (_) => new HomeScreen(),
+      },
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 400,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('images/background.png'),
+                          fit: BoxFit.fill)),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: 30,
+                        width: 80,
+                        height: 200,
+                        child: FadeAnimation(
+                            1,
+                            Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage('images/light-1.png'))),
+                            )),
+                      ),
+                      Positioned(
+                        left: 140,
+                        width: 80,
+                        height: 150,
+                        child: FadeAnimation(
+                            1.3,
+                            Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage('images/light-2.png'))),
+                            )),
+                      ),
+                      Positioned(
+                        right: 40,
+                        top: 40,
+                        width: 80,
+                        height: 150,
+                        child: FadeAnimation(
+                            1.5,
+                            Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage('images/clock.png'))),
+                            )),
+                      ),
+                      Positioned(
+                        child: FadeAnimation(
+                            1.6,
+                            Container(
+                              margin: EdgeInsets.only(top: 50),
+                              child: Center(
+                                child: Text(
+                                  "REGISTER",
+                                  style: TextStyle(
+                                      fontFamily: 'MontserratAlternates',
+                                      color: Colors.white,
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    children: <Widget>[
+                      FadeAnimation(
+                          1.8,
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Color.fromRGBO(143, 148, 251, .2),
+                                      blurRadius: 20.0,
+                                      offset: Offset(0, 10))
+                                ]),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              color: Colors.grey[100]))),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Email or Phone number",
+                                        hintStyle: TextStyle(
+                                            fontFamily: 'MontserratAlternates',
+                                            color: Colors.grey[400])),
+                                    keyboardType: TextInputType.emailAddress,
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              color: Colors.grey[100]))),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Password",
+                                        hintStyle: TextStyle(
+                                            fontFamily: 'MontserratAlternates',
+                                            color: Colors.grey[400])),
+                                    obscureText: true,
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "Confirm Password",
+                                        hintStyle: TextStyle(
+                                            fontFamily: 'MontserratAlternates',
+                                            color: Colors.grey[400])),
+                                    obscureText: true,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      FadeAnimation(
+                          2,
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacementNamed(
+                                  context, '/homescreen');
+                            },
+                            child: Container(
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  gradient: LinearGradient(colors: [
+                                    Color.fromRGBO(143, 148, 251, 1),
+                                    Color.fromRGBO(143, 148, 251, .6),
+                                  ])),
+                              child: Center(
+                                child: Text(
+                                  "REGISTER",
+                                  style: TextStyle(
+                                      fontFamily: 'MontserratAlternates',
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          )),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      FadeAnimation(
+                          1.5,
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacementNamed(context, '/loginscreen');
+                            },
+                            child: Text(
+                              "Already have an account?",
+                              style: TextStyle(
+                                  fontFamily: 'MontserratAlternates',
+                                  color: Color.fromRGBO(143, 148, 251, 1),
+                                  decoration: TextDecoration.underline),
+                            ),
+                          )),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
