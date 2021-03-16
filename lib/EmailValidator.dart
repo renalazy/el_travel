c3bcb22127b89c4159335807d8edcb61b6157da9
renalazy <renalazyy@gmail.com>
2021-03-17 00:03:47 +0700

    Initial Commit

---
+++ lib/EmailValidator.dart
@@ -0,0 +1,10 @@
+String emailValidator(String value) {
+  Pattern pattern =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regex = new RegExp(pattern);
+  if (value.isEmpty) return '*Required';
+  if (!regex.hasMatch(value))
+    return '*Enter a valid email';
+  else
+    return null;
+}
