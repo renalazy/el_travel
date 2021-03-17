03663f1d3b5e38f2d9fae0eabf95e8ec7bc6d946
renalazy <renalazyy@gmail.com>
2021-03-18 00:53:58 +0700

    Fixed

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,15 +1,29 @@
+import 'package:eltravel/loginScreen.dart';
 import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      routes: {
+        '/loginscreen': (_) => new LoginScreen(),
+      },
+    );
+  }
+}
+
+
 class HomeScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Screen'),
       ),
-      body: Center(
-        child: Text('First Screen'),
-      ),
+      body: Text("Home Screen"),
     );
   }
 }
